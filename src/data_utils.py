8a730ee72407d9cbd13c284e2982a15f6e0a5985
xxm <ximingxing@gmail.com>
2019-04-24 23:36:19 +0800

    batch_flow_bucket done

--- src/data_utils.py
+++ src/data_utils.py
@@ -117,5 +117,103 @@ def batch_flow(data, ws, batch_size, raw=False, add_end=True):
         batches = [np.asarray(x) for x in batches]
         yield batches
 
+
+def batch_flow_bucket(data, ws, batch_size, raw=False, add_end=True, n_bucket=5, bucket_ind=1, debug=False):
+    """
+    数据切分
+    :param data:
+    :param ws:
+    :param batch_size:
+    :param raw:
+    :param add_end:
+    :param n_bucket: 把数据分成了多少个bucket
+    :param bucket_ind: 哪一个维度的输入作为bucket的依据
+    :param debug:
+    :return:
+    """
+    all_data = list(zip(*data))
+    lengths = sorted(list(set([len(x[bucket_ind]) for x in all_data])))
+    if n_bucket > len(lengths):
+        n_bucket = len(lengths)
+
+    splits = np.array(lengths)[
+        (np.linspace(0, 1, 5, endpoint=False) * len(lengths)).astype(int)
+    ].tolist()
+
+    splits += [np.inf]  # np.inf无限大的正整数
+
+    if debug:
+        print(splits)
+
+    ind_data = {}
+    for x in all_data:
+        l = len(x[bucket_ind])
+        for ind, s in enumerate(splits[:-1]):
+            if l >= s and l <= splits[ind + 1]:
+                if ind not in ind_data:
+                    ind_data[ind] = []
+                ind_data[ind].append(x)
+                break
+
+    inds = sorted(list(ind_data.keys()))
+    ind_p = [len(ind_data[x]) / len(all_data) for x in inds]
+    if debug:
+        print(np.sum(ind_p), ind_p)
+
+    if isinstance(ws, (list, tuple)):
+        assert len(ws) == len(data), "len(ws) 必须等于len(data)，ws是list或者是tuple"
+
+    if isinstance(add_end, bool):
+        add_end = [add_end] * len(data)
+    else:
+        assert (isinstance(add_end, (list, tuple))), "add_end 不是 boolean，就应该是一个list(tuple) of boolean"
+        assert len(add_end) == len(data), "如果add_end 是list(tuple)，那么add_end的长度应该和输入数据长度是一致的"
+
+    mul = 2
+    if raw:
+        mul = 3
+
+    while True:
+        choice_ind = np.random.choice(inds, p=ind_p)
+        if debug:
+            print('choice_ind', choice_ind)
+        data_batch = random.sample(ind_data[choice_ind], batch_size)
+        batches = [[] for i in range(len(data) * mul)]
+
+        max_lens = []
+        for j in range(len(data)):
+            max_len = max([
+                len(x[j]) if hasattr(x[j], '__len__') else 0
+                for x in data_batch
+            ]) + (1 if add_end[j] else 0)
+
+            max_lens.append(max_len)
+
+        for d in data_batch:
+            for j in range(len(data)):
+                if isinstance(ws, (list, tuple)):
+                    w = ws[j]
+                else:
+                    w = ws
+
+                # 添加结尾
+                line = d[j]
+                if add_end[j] and isinstance(line, (tuple, list)):
+                    line = list(line) + [WordSequence.END_TAG]
+
+                if w is not None:
+                    x, xl = transform_sentence(line, w, max_lens[j], add_end[j])
+                    batches[j * mul].append(x)
+                    batches[j * mul + 1].append(xl)
+                else:
+                    batches[j * mul].append(line)
+                    batches[j * mul + 1].append(line)
+                if raw:
+                    batches[j * mul + 2].append(line)
+        batches = [np.asarray(x) for x in batches]
+
+        yield batches
+
+
 if __name__ == '__main__':
     print(_get_available_gpus())
